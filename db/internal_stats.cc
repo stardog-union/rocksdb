aaed15d92cfff878b235597856625d84ecd52105
Simon Grätzer <simon.gratzer@stardog.com>
2021-04-08 22:50:33 +0200

    Create DB properties for internal table cache

--- db/internal_stats.cc
+++ db/internal_stats.cc
@@ -258,6 +258,8 @@ static const std::string estimate_oldest_key_time = "estimate-oldest-key-time";
 static const std::string block_cache_capacity = "block-cache-capacity";
 static const std::string block_cache_usage = "block-cache-usage";
 static const std::string block_cache_pinned_usage = "block-cache-pinned-usage";
+static const std::string tablecache_capacity = "table-cache-capacity";
+static const std::string tablecache_usage = "table-cache-usage";
 static const std::string options_statistics = "options-statistics";
 
 const std::string DB::Properties::kNumFilesAtLevelPrefix =
@@ -346,6 +348,10 @@ const std::string DB::Properties::kBlockCacheUsage =
     rocksdb_prefix + block_cache_usage;
 const std::string DB::Properties::kBlockCachePinnedUsage =
     rocksdb_prefix + block_cache_pinned_usage;
+const std::string DB::Properties::kTableCacheCapacity =
+    rocksdb_prefix + tablecache_capacity;
+const std::string DB::Properties::kTableCacheUsage =
+    rocksdb_prefix + tablecache_usage;
 const std::string DB::Properties::kOptionsStatistics =
     rocksdb_prefix + options_statistics;
 
@@ -483,6 +489,12 @@ const std::unordered_map<std::string, DBPropertyInfo>
           nullptr}},
         {DB::Properties::kBlockCachePinnedUsage,
          {false, nullptr, &InternalStats::HandleBlockCachePinnedUsage, nullptr,
+          nullptr}},
+        {DB::Properties::kTableCacheCapacity,
+         {false, nullptr, &InternalStats::HandleTableCacheCapacity, nullptr,
+          nullptr}},
+        {DB::Properties::kTableCacheUsage,
+         {false, nullptr, &InternalStats::HandleTableCacheUsage, nullptr,
           nullptr}},
         {DB::Properties::kOptionsStatistics,
          {false, nullptr, nullptr, nullptr,
@@ -943,6 +955,18 @@ bool InternalStats::HandleBlockCachePinnedUsage(uint64_t* value, DBImpl* /*db*/,
     return false;
   }
   *value = static_cast<uint64_t>(block_cache->GetPinnedUsage());
+  return true;
+}
+
+bool InternalStats::HandleTableCacheCapacity(uint64_t* value, DBImpl* db,
+                                             Version* /*version*/) {
+  *value = static_cast<uint64_t>(db->table_cache_->GetCapacity());
+  return true;
+}
+
+bool InternalStats::HandleTableCacheUsage(uint64_t* value, DBImpl* db,
+                                          Version* /*version*/) {
+  *value = static_cast<uint64_t>(db->table_cache_->GetUsage());
   return true;
 }
 
